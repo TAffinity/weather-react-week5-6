ce9b3f5c1b4a7fa957fa10ddf72c6640fefae752
TAffinity <134305520+TAffinity@users.noreply.github.com>
2023-11-15 16:23:00 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
-      <p></p>
+      <p>
+        <button className="btn btn-primary">Нажми меня</button>
+      </p>
       <footer>
         Created by Tatiana Bugulova using React and{" "}
         <a
-          href="https://github.com/TAffinity/weather-react"
+          href="https://github.com/TAffinity/weather-react-week5-6"
           target="_blank"
           rel="noreferrer"
         >
