01a07fbaa5e7dd64bbe21aaecb9ecc347c363172
TAffinity <134305520+TAffinity@users.noreply.github.com>
2023-11-20 14:32:25 +0100

    change buttons color

--- src/Weather.js
+++ src/Weather.js
@@ -60,7 +60,7 @@ export default function Weather(props) {
               <input
                 type="submit"
                 value="Search"
-                className="btn btn-success w-100"
+                className="btn btn-primary w-100"
               />
             </div>
           </div>
