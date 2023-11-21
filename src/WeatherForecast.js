60fc7b0e173e5a2c18240063631cae687fdeae1d
TAffinity <134305520+TAffinity@users.noreply.github.com>
2023-11-21 19:11:53 +0100

    added API for Forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,12 +1,24 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
 export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let lon = props.coordinates.lon;
+  let lat = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
-        <div className="col-5">
+        <div className="col">
           <div className="WeatherForecast-day">Thu</div>
           <WeatherIcon code="01d" size={36} />
           <div className="WeatherForecast-temperature">
